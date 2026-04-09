9f7eb7fcc3fb9397ccfad8ea80b20fc50b0f3db2
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-04-09 19:28:22 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-primary text-white py-6 mt-16">
       <div className="max-w-7xl mx-auto text-center text-sm">
-        © {new Date().getFullYear()} Liberty Consultancy. All rights reserved.
+        © {new Date().getFullYear()} Nottingham It Solutions. All rights reserved.
       </div>
     </footer>
   )
